2a0d617528129e6b4bb6c6f23bfd54444ee45f9f
Gil Shwartz <58849439+GShwartz@users.noreply.github.com>
2022-08-04 13:47:38 +0300

    Update providers.tf

--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -16,9 +16,9 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id   = "25ce029c-138c-4b4c-9626-2b721b42cc07"
-  tenant_id         = "fb0457bd-2dee-4de3-856c-cdc97c8ad092"
-  client_id         = "0066ad24-af35-4cd3-a496-fa12bc08c971"
-  client_secret     = "9xA8Q~Hh51HORUHPFwZ-A2z6qM0hdcHuHW_sMcFI"
+  subscription_id   = ""
+  tenant_id         = ""
+  client_id         = ""
+  client_secret     = ""
 
 }
